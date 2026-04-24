ac2b24825d0ab759586cc6ccbbe81d7cef6d46eb
Jaya Praveena S <jayapraveenas@genworx.ai>
2026-04-24 22:09:46 +0530

    aded the destroy plan

--- modules/service_account/main.tf
+++
@@ -1,5 +0,0 @@
-resource "google_service_account" "main" {
-  account_id   = var.account_id
-  display_name = var.display_name
-  description  = "Service account for Cloud Run and database access"
-}
--- modules/service_account/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "account_id" {
-  type = string
-}
-variable "display_name" {
-  type = string
-}
